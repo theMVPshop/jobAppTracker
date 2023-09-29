3c893e5258a06cc33a7b3567f16fb9c84c199534
DKYoungOne <bawsinator@gmail.com>
2023-09-28 17:34:04 -0700

    Add basic login functionality for testing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,32 @@
 import './App.css'
+import { Auth0Provider } from '@auth0/auth0-react'
+import { useAuth0 } from '@auth0/auth0-react'
+import Profile from './components/Profile'
 import UploadFile from './UploadFile'
+import Authentication from './components/Authentication'
+
 
 function App() {
-
   return (
+
     <>
-      <UploadFile />
+      {/* Auth0Provider needs to wrap around the entire application, based on recommendation from Auth0 */}
+      <Auth0Provider
+        domain="dev-qxzngmucus86xphq.us.auth0.com"
+        clientId="s6f9hSpUxFolZsfQ7sNhD9JIO2s39fYl"
+        authorizationParams={{
+          redirect_uri: window.location.origin
+        }}
+      >
+        <Authentication />
+        <Profile />
+        <UploadFile />
+      </Auth0Provider>
+
     </>
   )
+
+
 }
 
 export default App
