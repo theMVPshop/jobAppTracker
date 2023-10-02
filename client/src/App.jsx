65eac7ddba5b228e04691ec6fc29553898958d8c
DKYoungOne <bawsinator@gmail.com>
2023-10-02 16:42:58 -0700

    Add env usage functionality to the authorization method.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
 import { Auth0Provider } from '@auth0/auth0-react'
-import { useAuth0 } from '@auth0/auth0-react'
 import Profile from './components/Profile'
 import UploadFile from './UploadFile'
 import Authentication from './components/Authentication'
+
+
 
 
 function App() {
@@ -12,8 +13,8 @@ function App() {
     <>
       {/* Auth0Provider needs to wrap around the entire application, based on recommendation from Auth0 */}
       <Auth0Provider
-        domain="dev-qxzngmucus86xphq.us.auth0.com"
-        clientId="s6f9hSpUxFolZsfQ7sNhD9JIO2s39fYl"
+        domain={import.meta.env.VITE_REACT_APP_DOMAIN_ID}
+        clientId={import.meta.env.VITE_REACT_APP_CLIENT_ID}
         authorizationParams={{
           redirect_uri: window.location.origin
         }}
