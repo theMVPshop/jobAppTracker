6c5565f705cb7678db49e21205d811a808099bfc
ConnorMcGehee <connorjames050@gmail.com>
2023-10-03 17:59:58 -0500

    Updated routes

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 import express from "express";
 import cors from "cors";
 import resumeRouter from "./server/routes/resume.js";
+import scrapeRouter from "./server/routes/scrape.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(routes)
 
 app.use("/api/resume", resumeRouter);
+app.use("/api/scrape", scrapeRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => {
