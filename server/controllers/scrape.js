a7778e9840f2b2032a1ab9e86b476d2445fb717e
ConnorMcGehee <connorjames050@gmail.com>
2023-10-04 18:03:03 -0500

    Added support for ZipRecruiter

--- server/controllers/scrape.js
+++ server/controllers/scrape.js
@@ -1,17 +1,17 @@
-import cheerio from 'cheerio';
-import puppeteer from 'puppeteer-extra';
-import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+import * as cheerio from "cheerio";
+import puppeteer from "puppeteer-extra";
+import StealthPlugin from "puppeteer-extra-plugin-stealth";
 
 export const scrape = async (req, res) => {
     try {
         const { url } = req.body;
-        const domain = new URL(url).hostname.split('.').slice(-2).join('.');  // Extracts the domain from the URL
+        const domain = new URL(url).hostname.split(".").slice(-2).join(".");  // Extracts the domain from the URL
         const parser = parsers[domain];
         const transformer = urlTransformers[domain];
-        if (!parser) return res.status(400).send('Unsupported domain');
+        if (!parser) return res.status(400).send("Website not supported.");
         
         const jobDescription = await fetchAndParse(url, parser, transformer);
-        return jobDescription ? res.status(200).send(jobDescription) : res.status(404).send('No job description found.');
+        return jobDescription ? res.status(200).send(jobDescription) : res.status(404).send("No job description found.");
 
     } catch (error) {
         console.error(error);
@@ -22,7 +22,7 @@ export const scrape = async (req, res) => {
 const fetchAndParse = async (url, parser, transformer) => {
     const browser = await puppeteer.use(StealthPlugin()).launch({ headless: true });
     const page = await browser.newPage();
-    await page.goto(transformer ? transformer(url) : url, { waitUntil: 'networkidle0' });
+    await page.goto(transformer ? transformer(url) : url, { waitUntil: "networkidle0" });
     const content = await page.content();
     await browser.close();
     const $ = cheerio.load(content);
@@ -31,27 +31,28 @@ const fetchAndParse = async (url, parser, transformer) => {
 
 const transformIndeedUrl = (url) => {
     const urlObj = new URL(url);
-    const vjkValue = new URLSearchParams(urlObj.search).get('vjk') || urlObj.searchParams.get('jk');
+    const vjkValue = new URLSearchParams(urlObj.search).get("vjk") || urlObj.searchParams.get("jk");
     return `https://www.indeed.com/viewjob?jk=${vjkValue}`;
 };
 
 const parseLinkedIn = ($) => {
-    return $('#job-details').text().trim() || $('.jobs-description').first().text().trim() || $(".show-more-less-html__markup").first().text().trim() || "";
+    return $("#job-details").text().trim() || $(".jobs-description").first().text().trim() || $(".show-more-less-html__markup").first().text().trim() || "";
 };
 
 const parseIndeed = ($) => {
-    return $('#jobDescriptionText').text().trim() || $(".jobsearch-jobDescriptionText").text().trim() || "";
+    return $("#jobDescriptionText").text().trim() || $(".jobsearch-jobDescriptionText").text().trim() || "";
 };
 
-// const parseZipRecruiter = ($) => { ... }
-// const transformZipRecruiterUrl = (url) => { ... }
+const parseZipRecruiter = ($) => { 
+    return $(".job_details").text().trim() || "";
+ }
 
 const parsers = {
-    'linkedin.com': parseLinkedIn,
-    'indeed.com': parseIndeed,
-    // 'ziprecruiter.com': parseZipRecruiter,
+    "linkedin.com": parseLinkedIn,
+    "indeed.com": parseIndeed,
+    "ziprecruiter.com": parseZipRecruiter,  
 };
 
 const urlTransformers = {
-    'indeed.com': transformIndeedUrl,
+    "indeed.com": transformIndeedUrl,
 };
