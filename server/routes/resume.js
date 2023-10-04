a731113acef85561589c468b258fd5afa5ce2cad
ConnorMcGehee <connorjames050@gmail.com>
2023-10-04 18:51:53 -0500

    GPT now returns a message upon resume upload

--- server/routes/resume.js
+++ server/routes/resume.js
@@ -1,11 +1,9 @@
 import express from "express";
-import { processResume, getEvent } from "../controllers/resume.js";
+import { processResume } from "../controllers/resume.js";
 import { upload } from "../middleware.js";
 
 const router = express.Router();
 
 router.post("/upload", upload.single("pdfFile"), processResume);
-
-router.get("/events/:fileId", getEvent);
 
 export default router;
