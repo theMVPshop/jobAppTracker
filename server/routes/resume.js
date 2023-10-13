bfbc383742e4dec523d34fdc8c7eeaa5b4ac82b3
ConnorMcGehee <connorjames050@gmail.com>
2023-10-12 19:38:21 -0500

    Added support for GET resume

--- server/routes/resume.js
+++ server/routes/resume.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { processResume, rateResume } from "../controllers/resume.js";
+import { uploadResume, getResume, rateResume } from "../controllers/resume.js";
 import { upload } from "../middleware.js";
 
 const router = express.Router();
 
-router.post("/upload", upload.single("pdfFile"), processResume);
+router.post("/users/:user_id/upload", upload.single("pdfFile"), uploadResume);
+router.get("/users/:user_id", getResume)
 router.post("/rate", rateResume);
 
 export default router;
