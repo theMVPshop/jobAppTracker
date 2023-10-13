a4a9e0c03a10455978b4f39b0d10750ade99ee06
ConnorMcGehee <connorjames050@gmail.com>
2023-10-12 19:37:56 -0500

    Released sql connections

--- server/routes/jobs.js
+++ server/routes/jobs.js
@@ -1,18 +1,11 @@
 import { Router } from 'express'
-import { createApplication, getUserApplications, updateApplication, deleteApplication } from '../controllers/applicationTracker.js'
+import { createApplication, getUserApplications, updateApplication, deleteApplication } from '../controllers/jobs.js'
 
 const router = Router()
 
-// Fetch all applications for a user
-router.get('/users/:user_id/applications', getUserApplications);
-
-// Create application record
 router.post('/users/:user_id/applications', createApplication);
-
-// Update job application
+router.get('/users/:user_id/applications', getUserApplications);
 router.put('/users/:user_id/applications/:application_id', updateApplication);
-
-// Delete a job application
 router.delete('/users/:user_id/applications/:application_id', deleteApplication);
 
 export default router;
