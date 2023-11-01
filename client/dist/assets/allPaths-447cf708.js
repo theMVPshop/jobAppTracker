51fe021c8d505a86f120cc40eb657f5c950d9c23
ConnorMcGehee <connorjames050@gmail.com>
2023-11-01 17:52:28 -0500

    Deploy

---
+++ client/dist/assets/allPaths-447cf708.js
@@ -0,0 +1,1 @@
+import{I as n}from"./index-2098b39e.js";import{I as e}from"./index-5fe2489d.js";import{p as r,I as s}from"./index-cf81e646.js";function I(o,t){var a=r(o);return t===s.STANDARD?n[a]:e[a]}function p(o){return r(o)}export{n as IconSvgPaths16,e as IconSvgPaths20,I as getIconPaths,p as iconNameToPathsRecordKey};
